593cff47e2e5f804dd667b14692dcd74fbd69627
Connor Macfarland <macfarct@mail.uc.edu>
2024-04-16 13:12:48 -0400

    Added easy Problem CS

---
+++ Group14_FinalProject/EasyProblem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Group14_FinalProject
+{
+    public class EasyProblem
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+                if (map.ContainsKey(complement))
+                {
+                    return new int[] { map[complement], i };
+                }
+                if (!map.ContainsKey(nums[i]))
+                {
+                    map[nums[i]] = i;
+                }
+            }
+            throw new ArgumentException("No two sum solution");
+        }
+
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 9;
+
+            int[] result = TwoSum(nums, target);
+
+            Console.WriteLine("Indices: " + result[0] + ", " + result[1]);
+        }
+    }
+}
+    
--- Group14_FinalProject/default.aspx
+++ Group14_FinalProject/default.aspx
@@ -9,7 +9,7 @@
 <body>
     <form id="form1" runat="server">
         <div>
-
+            <asp:Label ID="CodeLabel" runat="server" TextMode="MultiLine"></asp:Label>
 
         </div>
     </form>
